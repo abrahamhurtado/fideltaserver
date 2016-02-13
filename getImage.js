5e6657a6ff55008606a4938d31c842165247468a
arhcstolen <arhc.hc@gmail.com>
2016-02-13 10:19:31 -0700

    Pls, send halp

--- getImage.js
+++ getImage.js
@@ -2,5 +2,5 @@ var path = require('path');
 var fs = require('fs');
 
 module.exports = function () {
-  return fs.readFileSync(path.resolve('image.txt'), 'base64');
+  return fs.readFileSync(path.resolve('image.txt'), 'utf-8');
 }
